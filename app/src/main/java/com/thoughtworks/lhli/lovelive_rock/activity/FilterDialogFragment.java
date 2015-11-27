aaa50ea22b026881a01b4a2b7f1d83399e15ff0c
Li Linhao <lhli@thoughtworks.com>
2015-11-27 17:00:12 +0800

    Modify the dialog style.

--- app/src/main/java/com/thoughtworks/lhli/lovelive_rock/activity/FilterDialogFragment.java
+++ app/src/main/java/com/thoughtworks/lhli/lovelive_rock/activity/FilterDialogFragment.java
@@ -1,16 +1,21 @@
 package com.thoughtworks.lhli.lovelive_rock.activity;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import com.thoughtworks.lhli.lovelive_rock.R;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class FilterDialogFragment extends DialogFragment {
 
@@ -35,43 +40,63 @@ public class FilterDialogFragment extends DialogFragment {
     private ArrayAdapter<CharSequence> adapter;
 
     @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        LayoutInflater inflater = getActivity().getLayoutInflater();
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        Window window = getDialog().getWindow();
+        window.setGravity(Gravity.TOP | Gravity.LEFT);
+        WindowManager.LayoutParams params = window.getAttributes();
+        params.x = 0;
+        params.y = 150;
+        window.setAttributes(params);
 
-        builder.setView(inflater.inflate(R.layout.filter_dialog, null));
-        return builder.create();
+        return inflater.inflate(R.layout.filter_dialog, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ButterKnife.bind(this, getView());
+        setDropDownList();
     }
 
     private void setDropDownList() {
-//        adapter = ArrayAdapter.createFromResource(this,
-//                R.array.rarity_array, android.R.layout.simple_spinner_item);
-//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        raritySpinner.setAdapter(adapter);
-//
-//        adapter = ArrayAdapter.createFromResource(this,
-//                R.array.idol_array, android.R.layout.simple_spinner_item);
-//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        idolSpinner.setAdapter(adapter);
-//
-//        adapter = ArrayAdapter.createFromResource(this,
-//                R.array.attribute_array, android.R.layout.simple_spinner_item);
-//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        attributeSpinner.setAdapter(adapter);
-//
-//        adapter = ArrayAdapter.createFromResource(this,
-//                R.array.grade_array, android.R.layout.simple_spinner_item);
-//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        gradeSpinner.setAdapter(adapter);
-//
-//        adapter = ArrayAdapter.createFromResource(this,
-//                R.array.sub_team_array, android.R.layout.simple_spinner_item);
-//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        subTeamSpinner.setAdapter(adapter);
-//
-//        adapter = ArrayAdapter.createFromResource(this,
-//                R.array.skill_array, android.R.layout.simple_spinner_item);
-//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        skillTypeSpinner.setAdapter(adapter);
+        adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.rarity_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        raritySpinner.setAdapter(adapter);
+
+        adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.idol_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        idolSpinner.setAdapter(adapter);
+
+        adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.attribute_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        attributeSpinner.setAdapter(adapter);
+
+        adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.grade_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        gradeSpinner.setAdapter(adapter);
+
+        adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.sub_team_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        subTeamSpinner.setAdapter(adapter);
+
+        adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.skill_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        skillTypeSpinner.setAdapter(adapter);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Dialog dialog = getDialog();
+        if (dialog != null) {
+            dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        }
     }
 }
