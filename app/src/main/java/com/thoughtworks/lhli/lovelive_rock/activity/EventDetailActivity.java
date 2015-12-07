aea1ab2272ce2f4c0e9225267e5581bac0d52162
Li Linhao <lhli@thoughtworks.com>
2015-12-07 14:39:15 +0800

    Added event detail layout.

---
+++ app/src/main/java/com/thoughtworks/lhli/lovelive_rock/activity/EventDetailActivity.java
@@ -0,0 +1,10 @@
+package com.thoughtworks.lhli.lovelive_rock.activity;
+
+import android.os.Bundle;
+
+public class EventDetailActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
