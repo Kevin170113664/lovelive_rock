b1cecc8bb5703ca76f9147643ff548d7e60cf7a6
Li Linhao <lhli@thoughtworks.com>
2015-12-09 16:23:09 +0800

    Set spinner adapter and refactor MF calculator activity.

--- app/src/main/java/com/thoughtworks/lhli/lovelive_rock/activity/MedleyFestivalCalculatorActivity.java
+++ app/src/main/java/com/thoughtworks/lhli/lovelive_rock/activity/MedleyFestivalCalculatorActivity.java
@@ -2,8 +2,10 @@ package com.thoughtworks.lhli.lovelive_rock.activity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.Spinner;
 
 import com.thoughtworks.lhli.lovelive_rock.R;
 
@@ -24,6 +26,20 @@ public class MedleyFestivalCalculatorActivity extends BaseActivity {
     @Bind(R.id.event_time)
     RelativeLayout eventTimeLayout;
 
+    @Bind(R.id.song_amount_spinner)
+    Spinner songAmountSpinner;
+
+    @Bind(R.id.difficulty_spinner)
+    Spinner difficultySpinner;
+
+    @Bind(R.id.song_rank_spinner)
+    Spinner songRankSpinner;
+
+    @Bind(R.id.combo_rank_spinner)
+    Spinner comboRankSpinner;
+
+    private ArrayAdapter<CharSequence> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +49,24 @@ public class MedleyFestivalCalculatorActivity extends BaseActivity {
         advancedOptionsLayout.setVisibility(View.GONE);
         eventTimeLayout.setVisibility(View.GONE);
 
+        setDropdownList();
+        setButtonOnClickListener();
+    }
+
+    private void setDropdownList() {
+        setSpinnerAdapter(songAmountSpinner, R.array.song_amount);
+        setSpinnerAdapter(difficultySpinner, R.array.difficulty);
+        setSpinnerAdapter(songRankSpinner, R.array.song_rank);
+        setSpinnerAdapter(comboRankSpinner, R.array.combo_rank);
+    }
+
+    private void setSpinnerAdapter(Spinner spinner, int resourceId) {
+        adapter = ArrayAdapter.createFromResource(this, resourceId, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+    private void setButtonOnClickListener() {
         advancedOptionsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
